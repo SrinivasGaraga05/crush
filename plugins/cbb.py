c53874dd96b1e688fb59ce11c1db4e8431b1d494
sainallamilli <142711611+sainallamilli@users.noreply.github.com>
2025-03-17 00:45:08 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,4 +1,4 @@
-#(©)Yugen_Bots
+# (©)Yugen_Bots
 
 from pyrogram import __version__
 from bot import Bot
@@ -8,40 +8,42 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
-    data = query.data
-    if data == "about":
-        abt_msg=f'<b>⟦★⟧ Hi There {mention}</b>!💫\n<b>┏━━━━━━━❪❂❫━━━━━━━━</b>\n◈ <b>ᴄʀᴇᴀᴛᴏʀ</b>: <b><a href="https://t.me/rai_yan_00">Rᴀɪ Yᴀɴ</a></b>\n◈ <b>ꜰᴏᴜɴᴅᴇʀ ᴏꜰ</b>: <b><a href="https://t.me/Ani_Weebs">ᴀɴɪᴍᴇ ᴡᴇᴇʙs</a></b>\n◈ <b>ᴏɴɢᴏɪɴɢ ᴄʜᴀɴɴᴇʟ</b>: <b><a href="https://t.me/Ongoing_Ani_Weebs">ᴏɴɢᴏɪɴɢ ᴡᴇᴇʙs</a></b>\n◈ <b>Lɪʙʀᴀʀʏ</b>: <b><a href="https://pyrogram.org">Pyʀᴏɢʀᴀᴍ</a></b>\n◈ <b>ᴍʏ ꜱᴇʀᴠᴇʀ</b>: <b><a href="https://render.com">Rᴇɴᴅᴇʀ</a></b>\n◈ <b>ᴅᴇᴠᴇʟᴏᴘᴇʀ</b>: <b><a href="https://t.me/voatcb">VØAT</a></b>\n<b>┗━━━━━━━❪❂❫━━━━━━━━</b>'
-        ABOUT_TXT = abt_msg.format(mention=query.from_user.mention)
-        await query.message.edit_text(
-            text = ABOUT_TXT,
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
-                [
-                    
-                    [
-                    InlineKeyboardButton("😔 ᴄʟᴏsᴇ", callback_data = "close")
-                    ]
-                ]
-            )
-        )
-    elif data == "help":
-        await query.message.edit_text(
-            text = HELP_MSG,
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
-                [
-                    [
-                    InlineKeyboardButton("😔 ᴄʟᴏsᴇ", callback_data = "close")
-                    ]
-                ]
-            )
-        )
-        
-        
-    elif data == "close":
-        await query.message.delete()
-        try:
-            await query.message.reply_to_message.delete()
-        except:
-            pass
+    data = query.data
+    if data == "about":
+        abt_msg = f'<b>⟦★⟧ Hi There {query.from_user.mention}</b>!\n' \
+                  f'<b>┏━━━━━━━❪❂❫━━━━━━━━</b>\n' \
+                  f'◈ <b>Dᴇᴠᴇʟᴏᴘᴇʀ</b>: <b><a href="https://t.me/LuffyDSunGodBot">ꜱᴀɪ ɴᴀʟʟᴀᴍɪʟʟɪ</a></b>\n' \
+                  f'◈ <b>Oɴɢᴏɪɴɢ Cʜᴀɴɴᴇʟ</b>: <b><a href="https://t.me/animes3u">Oɴɢᴏɪɴɢ Aɴɪᴍᴇ</a></b>\n' \
+                  f'◈ <b>Mᴀɪɴ Cʜᴀɴɴᴇʟ</b>: <b><a href="https://t.me/animes2u">Mᴀɪɴ Cʜᴀɴɴᴇʟ</a></b>\n' \
+                  f'<b>┗━━━━━━━❪❂❫━━━━━━━━</b>'
+        await query.message.edit_text(
+            text=abt_msg,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")
+                    ]
+                ]
+            )
+        )
 
+    elif data == "help":
+        await query.message.edit_text(
+            text=HELP_MSG,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")
+                    ]
+                ]
+            )
+        )
+
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
